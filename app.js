40b54852012f18d4fcc25101218e30d51af16a3e
projektoscio-png <projekto.scio@gmail.com>
2026-09-14 19:31:30 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const GOOGLE_CLIENT_ID = '292792599906-9m3t841hk507s1k042193tjuigoe1svb.apps.goo
 const DRIVE_SCOPE = 'https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/calendar.events';
 const DRIVE_FILE_NAME = 'mi-horario-sync.json';
 const DRIVE_PHOTOS_FILE_NAME = 'mi-horario-fotos.json';
-const APP_VERSION = '2026-08-22-31';
+const APP_VERSION = '2026-08-22-32';
 const DOW = ['Lunes','Martes','Miércoles','Jueves','Viernes','Sábado','Domingo'];
 const DOW_SHORT = ['L','M','X','J','V','S','D'];
 const SUBJECT_COLORS = ['#457B9D','#E76F51','#2A9D8F','#E9C46A','#7B6D8E','#D65A5A','#6A8D73','#9C6644','#3A86FF','#B5838D'];
@@ -1459,7 +1459,14 @@ function bindContentEvents(){
     el.onclick=(e)=>{ e.stopPropagation(); openClassOccurrenceModal(el.dataset.openClass, el.dataset.date); };
   });
   document.querySelectorAll('[data-open-item]').forEach(el=>{
-    el.onclick=()=>openItemModal(el.dataset.openItem);
+    el.onclick=()=>{
+      const item = state.items.find(i=>i.id===el.dataset.openItem);
+      if(item && item.kind==='deberes'){
+        openDeberesModal(item.id, item.subjectId, item.date, item.classId);
+      } else {
+        openItemModal(el.dataset.openItem);
+      }
+    };
   });
   document.querySelectorAll('[data-open-holiday]').forEach(el=>{
     el.onclick=()=>openHolidayModal(el.dataset.openHoliday);
